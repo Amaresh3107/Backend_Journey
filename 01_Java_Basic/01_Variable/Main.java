4bc7e67f1a525b2506c20b4baf9d4b86aab021d7
amaresh <amaresh9973kumar@gmail.com>
2025-04-05 17:06:13 +0530

    java basic

---
+++ 01_Java_Basic/01_Variable/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    
+	public static void main(String[] args) {
+		System.out.print(3+5);
+		System.out.print(8+7);
+		
+		//int num1=3;
+		//int num2=5;
+		//System.out.println(num1+num2);
+		
+		int num1=3;
+		int num2=5;
+		int result=num1+num2;
+		System.out.println(result);
+	}
+}
